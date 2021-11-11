be0e3251f0d5e55b50a39cfddb1c2a626717638f
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-11 14:46:35 +0300

    ststic props for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import { IParseSong, ISong } from '../types/IParseSong'
+import type { InferGetStaticPropsType } from 'next'
+import { IParseSong, ISong } from '../types/ISong'
 
-interface PropType {
-  resArr: IParseSong[]
-}
+import { promises as fs } from 'fs'
+import path from 'path'
 
-const Home = ({resArr}: PropType) => {
+const Home = ({resArr}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="bg-red-300 ">
       <ul>
         {
           resArr.map((e, index) => {
             return (
-              <li>
-                <span>{index}. </span>
-                <p>{e.name}</p>
-                <p>{e.code}</p>
+              <li key={index}>
+                <p><span>{index}. </span> {e?.name} - {e?.code}</p>
+                <iframe src={`https://open.spotify.com/embed/track/${e?.id}`}></iframe>
               </li>
             )
           })
@@ -33,19 +31,48 @@ function parseName(name: string): string {
   return result
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps = async ( ) => {
   const res = await fetch(`https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0`)
   const data: ISong[] = await res.json()
 
-  const resArr: IParseSong[] = data.splice(0, 10).map(e => {
-    return {
-      id: Date.now(),
-      name: e.value.nameofthesong ? parseName(e.value.nameofthesong) : '',
-      code: +e.value.robloxcode,
-    }
-  })
+  const apiKeyFile = path.join(process.cwd(), '/api_key_spotify/key.txt')
+  const apiKey = await fs.readFile(apiKeyFile, 'utf-8')
 
-  return { props: { resArr } }
+  const resArr: (IParseSong | null)[] = await Promise.all(data.splice(0, 20).map( async (e) => {
+    const songName = e.value.nameofthesong ? parseName(e.value.nameofthesong) : ''
+    const songCode = +e.value.robloxcode
+
+    try {
+      const searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${songName}&type=track&limit=1`, {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'ANYTHING_WILL_WORK_HERE',
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        },
+      })
+      const searchData = await searchResponse.json()
+
+      if (searchData.tracks?.items[0]) {
+        return {
+          id: searchData.tracks.items[0].id,
+          name: songName,
+          code: songCode,
+        }
+      } else {
+        return null
+      }
+    } catch (error) {
+      console.log('error', error);
+      return null
+    } 
+  })) 
+
+  return { 
+    props: { resArr: resArr.filter(e => !!e) },
+    revalidate: 3500,
+  }
 }
 
 // https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0
