6bc46375b106a5e8a4770c873ae731955df56506
Ultraviolence322 <segeichenkov18@gmail.com>
2021-11-13 12:22:18 +0300

    updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,59 @@
 import type { InferGetStaticPropsType } from 'next'
-import { IParseSong, ISong } from '../types/ISong'
+import { IParsedSong, ISong } from '../types/ISong'
 
 import { promises as fs } from 'fs'
 import path from 'path'
+import { useEffect, useState } from 'react'
+import { fetchNewSong } from '../helpers/fetchNewSong'
+import { parseName } from '../helpers/parseName'
 
-const Home = ({resArr}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = ({allSongs, apiKey}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [readySongs, setReadySongs] = useState<IParsedSong[]>([])
+  const countOfSongs: number = allSongs.length
+
+  useEffect(() => {
+    const getData = async () => {
+      let songs: IParsedSong[] = []
+
+      while(songs.length < 3) {
+        const fetchedSong = await fetchNewSong(allSongs, countOfSongs, apiKey)
+        if(fetchedSong) {
+          songs.push(fetchedSong)
+        }
+      }
+
+      setReadySongs(songs)
+    }
+    
+    getData()
+  }, [])
+
+  const prevSong = () => {
+
+  } 
+
+  const nextSong = async () => {
+    const fetchedSong: IParsedSong | null = await fetchNewSong(allSongs, countOfSongs, apiKey)
+
+    if(fetchedSong) {
+      setReadySongs([...readySongs, fetchedSong])
+    }
+  } 
+  
+  console.log('readySongs', readySongs);
+  
   return (
     <div className="bg-red-300 ">
+      <div>
+        <button onClick={prevSong}>prev</button>
+        <button onClick={nextSong}>next</button>
+      </div>
       <ul>
         {
-          resArr.map((e, index) => {
+          readySongs.map((e: any, index: any) => {
             return (
               <li key={index}>
-                <p><span>{index}. </span> {e?.name} - {e?.code}</p>
+                <p><span>{index}. </span> {e.songName} - {e.songCode}</p>
                 <iframe src={`https://open.spotify.com/embed/track/${e?.id}`}></iframe>
               </li>
             )
@@ -23,63 +64,44 @@ const Home = ({resArr}: InferGetStaticPropsType<typeof getStaticProps>) => {
   )
 }
 
-function parseName(name: string): string {
-  let result: string = ''
-  let splittedName = name.split('>')
-  result = splittedName[1].split('').splice(0, splittedName[1].length - 3).join('')
-
-  return result
-}
-
 export const getStaticProps = async ( ) => {
-  const res = await fetch(`https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0`)
-  const data: ISong[] = await res.json()
+  const urls = ['https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0',
+    'https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0',
+    'https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=1&ninja_table_public_nonce=3a5901169d',
+    'https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=2&ninja_table_public_nonce=3a5901169d',
+    'https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=3&ninja_table_public_nonce=3a5901169d',
+  ]
+  let allSongs: IParsedSong[] = [] 
 
-  const apiKeyFile = path.join(process.cwd(), '/api_key_spotify/key.txt')
-  const apiKey = await fs.readFile(apiKeyFile, 'utf-8')
-
-  const resArr: (IParseSong | null)[] = await Promise.all(data.splice(0, 20).map( async (e) => {
-    const songName = e.value.nameofthesong ? parseName(e.value.nameofthesong) : ''
-    const songCode = +e.value.robloxcode
-
+  await Promise.all(urls.map(async (url) => {
     try {
-      const searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${songName}&type=track&limit=1`, {
-        method: 'GET',
-        headers: {
-          'User-Agent': 'ANYTHING_WILL_WORK_HERE',
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        },
-      })
-      const searchData = await searchResponse.json()
-      console.log('searchData', searchData);
-      
+      const songsResponse = await fetch(url)
+      const songs: ISong[] = await songsResponse.json() 
+      const parsedArray: IParsedSong[] = []
 
-      if (searchData.tracks?.items[0]) {
-        return {
-          id: searchData.tracks.items[0].id,
-          name: songName,
-          code: songCode,
-        }
-      } else {
-        return null
-      }
+      songs.splice(0, 20).forEach(song => {
+        const songName = song.value.nameofthesong ? parseName(song.value.nameofthesong) : ''
+        const songCode = +song.value.robloxcode.replace('<k1>', '').replace('</k1>', '')
+
+        parsedArray.push({
+          songName,
+          songCode
+        })
+      })
+
+      allSongs = [...allSongs, ...parsedArray]
     } catch (error) {
       console.log('error', error);
-      return null
-    } 
-  })) 
+    }
+  }))
+
+  const apiKeyFile = path.join(process.cwd(), '/api_key_spotify/key.txt')
+  const apiKey: string = await fs.readFile(apiKeyFile, 'utf-8')
 
   return { 
-    props: { resArr: resArr.filter(e => !!e) },
+    props: { allSongs, apiKey },
     revalidate: 3600,
   }
 }
-
-// https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&ninja_table_public_nonce=3a5901169d&chunk_number=0
-// https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=1&ninja_table_public_nonce=3a5901169d
-// https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=2&ninja_table_public_nonce=3a5901169d
-// https://robloxmusics.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=44265&target_action=get-all-data&default_sorting=new_first&skip_rows=0&limit_rows=0&chunk_number=3&ninja_table_public_nonce=3a5901169d
 
 export default Home
